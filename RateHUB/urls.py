5c9e87d6ef798f095ecd48cdd96f4b8a4d28d3de
Arthur von Sohsten <arthurdelimavonsohsten@gmail.com>
2025-04-04 14:45:38 -0300

    Corrigindo conflito no arquivo urls.py

--- RateHUB/urls.py
+++ RateHUB/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from Aplicativo import views
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('Home_Admin/',views.homeAdmin, name='homeAdminNomePath'),
-    path('Home_User/',views.homeUser, name='homeUserNomePath'),
+    path('Home_Admin/', views.homeAdmin, name='homeAdminNomePath'),
+    path('Home_User/', views.homeUser, name='homeUserNomePath'),
     path('', views.login, name='loginNomePath'),
     path('Cadastro/', views.cadastro, name='cadastroNomePath'),
     path('Adicionar_Filmes_Admin/', views.filmes, name='adcFilmesAdmNomePath'),
@@ -13,8 +13,4 @@ urlpatterns = [
     path('meus-favoritos/', views.meus_favoritos, name='meus_favoritos'),
     path('logout/', views.logout_view, name='logout'),
     path('Banco_Dados/', admin.site.urls),
-=======
-    path('admin/', admin.site.urls),
-    path('', include('Aplicativo.urls')),  # Substitua "core" pelo nome real da sua app
->>>>>>> 9cee99f345fbef577cba6acd3aef7d9ecaecf339
 ]
