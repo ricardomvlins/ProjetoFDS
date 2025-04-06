35915be9b10187ad5b6aaa673953de567543f4b3
GugaPedrosa <169106326+GugaPedrosa@users.noreply.github.com>
2025-04-06 20:30:02 -0300

    Configura uso de PostgreSQL no Heroku

--- RateHUB/settings.py
+++ RateHUB/settings.py
@@ -75,11 +75,9 @@ WSGI_APPLICATION = 'RateHUB.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600
-    )
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
